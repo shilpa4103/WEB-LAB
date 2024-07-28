6db70cb16b688f71f7949ff3e9f2434cc9895451
shilpa4103 <shilpashree4103@gmail.com>
2024-07-28 21:08:50 +0530

    Done

---
+++ 10/10a.php
@@ -0,0 +1,4 @@
+<?php
+$today_date=getdate();
+echo "<h1>Today date is $today_date[mday]/$today_date[mon]/$today_date[year]</h1>"
+?>
---
+++ 10/10b.php
@@ -0,0 +1,23 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $num=$_POST["num"];
+    if($num==1||$num==0){
+        echo "NUmber is neigther prime nor not prime";
+        return;
+
+    }
+    $flag=0;
+    for($i=2;$i<=sqrt($num);$i++){
+        if($num%$i==0){
+            $flag=1;
+            break;
+        }
+    }
+    if($flag==1){
+        echo "Number is not a prime number";
+    }
+    else
+    echo "The number is prime";
+
+}
+?>
